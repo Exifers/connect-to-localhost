adc703e69b048ea0ceb6ea4a1981b2f04b932dc7
Emmanuel <meric.emmanuel@gmail.com>
2020-03-23 10:54:39 +0100

    simple localhost:3000 request

---
+++ build/precache-manifest.11b240ae7fa1f4a44ea79b084b00150f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169d403716222887d4330a6249e463a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49a84e40d4fb2dce76f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d076d3bfb4a9ae4349ad",
+    "url": "/static/js/2.95a97d30.chunk.js"
+  },
+  {
+    "revision": "9545d2ff040f1d230f20c359158fcfad",
+    "url": "/static/js/2.95a97d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49a84e40d4fb2dce76f",
+    "url": "/static/js/main.f109c55f.chunk.js"
+  },
+  {
+    "revision": "ccb9472fa046affb1712",
+    "url": "/static/js/runtime-main.5502ecd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {get} from 'fetch-factorized';
 
 function App() {
+
+  useEffect(() => {
+    get('http://localhost:3000/').then(console.log);
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
